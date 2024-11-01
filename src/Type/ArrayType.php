101e42e49c287ae70deff45d1219afd9d4bcaba8
Paul M. Jones <pmjones@pmjones.io>
2024-11-01 15:51:41 -0500

    Introduce uppercase-string

--- src/Type/ArrayType.php
+++ src/Type/ArrayType.php
@@ -368,6 +368,11 @@ class ArrayType implements Type
 		return TrinaryLogic::createNo();
 	}
 
+	public function isUppercaseString(): TrinaryLogic
+	{
+		return TrinaryLogic::createNo();
+	}
+
 	public function isClassStringType(): TrinaryLogic
 	{
 		return TrinaryLogic::createNo();
