ff999299810056a8cb419f818a0bb85e0697b758
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-12-22 11:23:05 +0100

    WIP

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -25,6 +25,8 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 	 */
 	private static function findTestFiles(): iterable
 	{
+		yield __DIR__ . '/nsrt/loose-comparisons.php'; return;
+
 		foreach (self::findTestDataFilesFromDirectory(__DIR__ . '/nsrt') as $testFile) {
 			yield $testFile;
 		}
